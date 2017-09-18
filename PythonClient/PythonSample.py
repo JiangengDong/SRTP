db2daf1c1e5213031d9f9db43845c4e04e5b02a7
DongJG <3150103642@zju.edu.cn>
2017-09-18 22:58:19 +0800

    co-debug

--- PythonClient/PythonSample.py
+++ PythonClient/PythonSample.py
@@ -1,16 +1,16 @@
 ﻿from NatNetClient import NatNetClient
 from ZJUVision import ZJUVisionServer
+import time
 
 
 # This is a callback function that gets connected to the NatNet client and called once per mocap frame.
 def receiveNewFrame(frameNumber, tracedata, latency, isRecording):
-    print("Received frame", frameNumber)
-
-
-# This is a callback function that gets connected to the NatNet client. It is called once per rigid body per frame
-def receiveRigidBodyFrame(id, position, rotation):
-    print("Received frame for rigid body", id)
-
+    # print("Received frame from Motive", frameNumber)
+    f = open('Motive', 'w')
+    try:
+        f.write(frameNumber, "from Motive: ", tracedata, " Latency: ", latency)
+    finally:
+        f.close()
 
 # This will create a new NatNet client
 serverIP = input("the IP address of server: ")
@@ -20,8 +20,28 @@ streamingClient = NatNetClient(serverIP, multicastIP, target)
 
 # Configure the streaming client to call our rigid body handler on the emulator to send data out.
 streamingClient.newFrameListener = receiveNewFrame
-streamingClient.rigidBodyListener = receiveRigidBodyFrame
+
+
+# This is a callback function that gets connected to the ZJUVisionServer. It is called once per frame.
+def receiveZJU(data, addr):
+    f = open('ZJUVision', 'w')
+    try:
+        f.write("Receive: ", data, " from: ", addr)
+    finally:
+        f.close()
+
+# Create a new ZJUVision client
+zjuvisionserver=ZJUVisionServer()
+
+# Configure the Server
+zjuvisionserver.newFrameListener=receiveZJU
 
 # Start up the streaming client now that the callbacks are set up.
 # This will run perpetually, and operate on a separate thread.
 streamingClient.run()
+zjuvisionserver.run()
+
+time.sleep(10)
+
+streamingClient.stop()
+zjuvisionserver.stop()
